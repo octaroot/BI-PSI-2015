6736c6825b6f8185c7193479d7d1852f661b96e9
Martin Černáč <me@martincernac.cz>
2015-03-06 22:09:11 +0100

    OutpuCommand can now also use streams

--- OutputCommand.java
+++ OutputCommand.java
@@ -1,9 +1,23 @@
+import java.io.PrintWriter;
+
 /**
  * @author martin (cernama9@fit.cvut.cz)
  * @since 6.3.15.
  */
 class OutputCommand {
     public enum MessageTypes {LOGIN, PASSWORD, OK, BAD_CHECKSUM, LOGIN_FAILED, SYNTAX_ERROR, TIMEOUT}
+
+    public static void sendMessage(String message, PrintWriter stream)
+    {
+        //mozna by slo pouzit println?
+        message += "\r\n";
+        stream.print(message);
+    }
+
+    public static void sendMessage(MessageTypes message, PrintWriter stream)
+    {
+        sendMessage(generateMessage(message), stream);
+    }
 
     public static String generateMessage(MessageTypes messageType) {
 
